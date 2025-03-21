a727f815929695ed12d3e56e8e8cb6a2a55016ab
jeet_mandal <jeet22761@gmail.com>
2025-03-21 19:43:28 +0530

    1st round complete

--- Frontend/src/Components/HeroSection.jsx
+++ Frontend/src/Components/HeroSection.jsx
@@ -77,23 +77,8 @@ const HeroSection = () => {
               animate={{ opacity: 1 }}
               transition={{ delay: 1.1 }}
             >
-              <div className="flex items-center text-green-500">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-5 w-5"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-                    clipRule="evenodd"
-                  />
-                </svg>
-              </div>
-              <p className="text-gray-400 text-sm">
-                No credit card required • 100% free to start
-              </p>
+              
+              
             </motion.div>
           </motion.div>
 
@@ -102,7 +87,7 @@ const HeroSection = () => {
             className="md:w-1/2 mt-12 md:mt-0 z-10 flex justify-center"
             initial={{ x: 0 }}
             animate={{
-              x: [100, 0, 100],
+              x: [10, 0, 10],
             }}
             transition={{
               duration: 3.6,
